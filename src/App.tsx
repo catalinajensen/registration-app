f1e0da1d0b63fc3f2f2f6341799c63302e5db42d
Catalina Jensen <catalinabjensen@gmail.com>
2023-06-13 22:46:04 +0200

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import Header from './components/Header';
 import RegistrationForm from './components/RegistrationForm';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RegistrationForm />
+  },
+  {
+    path: '/users',
+    element: <>users</>
+  }
+]);
 
 function App() {
   return (
     <>
       <Header />
-      <RegistrationForm />
+      <RouterProvider router={router} />
     </>
   );
 }
